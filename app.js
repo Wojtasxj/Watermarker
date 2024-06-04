85bd0206491996192b0b0f73aa09a731344bac79
Wojciech Klause <wojtasxj@gmail.com>
2024-06-04 18:14:34 +0200

    Watermarker works

--- app.js
+++ app.js
@@ -1,10 +1,115 @@
 const Jimp = require('jimp');
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
-  const image = await Jimp.read(inputFile);
-  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-  image.print(font, 10, 10, text);
-  await image.quality(100).writeAsync(outputFile);
+async function addTextWatermarkToImage(inputFile, outputFile, text) {
+    try {
+        if (!fs.existsSync(inputFile)) {
+            throw new Error('Plik źródłowy nie istnieje!');
+        }
+
+        const image = await Jimp.read(inputFile);
+        const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+        const textData = {
+            text,
+            alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+            alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+        };
+
+        image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
+        await image.quality(100).writeAsync(outputFile);
+
+        console.log('Znak wodny tekstu został dodany pomyślnie!');
+        startApp();
+    } catch (error) {
+        console.error('Wystąpił problem: ', error.message);
+        console.log('Spróbuj ponownie.');
+    }
+}
+
+async function addImageWatermarkToImage(inputFile, outputFile, watermarkFile) {
+    try {
+        if (!fs.existsSync(inputFile)) {
+            throw new Error('Plik źródłowy nie istnieje!');
+        }
+
+        if (!fs.existsSync(watermarkFile)) {
+            throw new Error('Plik znaku wodnego nie istnieje!');
+        }
+
+        const image = await Jimp.read(inputFile);
+        const watermark = await Jimp.read(watermarkFile);
+        const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+        const y = image.getHeight() / 2 - watermark.getHeight() / 2;
+
+        image.composite(watermark, x, y, {
+            mode: Jimp.BLEND_SOURCE_OVER,
+            opacitySource: 0.5,
+        });
+        await image.quality(100).writeAsync(outputFile);
+
+        console.log('Znak wodny obrazu został dodany pomyślnie!');
+        startApp();
+    } catch (error) {
+        console.error('Wystąpił problem: ', error.message);
+        console.log('Spróbuj ponownie.');
+    }
+}
+
+const startApp = async () => {
+    const answer = await inquirer.prompt([
+        {
+            name: 'start',
+            message: 'Hi! Welcome to "Watermark manager". Copy your image files to `/img` folder. Then you\'ll be able to use them in the app. Are you ready?',
+            type: 'confirm',
+        },
+    ]);
+
+    if (!answer.start) {
+        process.exit();
+    }
+
+    const options = await inquirer.prompt([
+        {
+            name: 'inputImage',
+            type: 'input',
+            message: 'What file do you want to mark?',
+            default: 'test.jpg',
+        },
+        {
+            name: 'watermarkType',
+            type: 'list',
+            choices: ['Text watermark', 'Image watermark'],
+        },
+    ]);
+
+    const prepareOutputFilename = (filename) => {
+        const [name, ext] = filename.split('.');
+        return `${name}-with-watermark.${ext}`;
+    };
+
+    if (options.watermarkType === 'Text watermark') {
+        const text = await inquirer.prompt([
+            {
+                name: 'value',
+                type: 'input',
+                message: 'Type your watermark text:',
+            },
+        ]);
+        options.watermarkText = text.value;
+        await addTextWatermarkToImage('./img/' + options.inputImage, './img/' + prepareOutputFilename(options.inputImage), options.watermarkText);
+    } else {
+        const image = await inquirer.prompt([
+            {
+                name: 'filename',
+                type: 'input',
+                message: 'Type your watermark name:',
+                default: 'logo.png',
+            },
+        ]);
+        options.watermarkImage = image.filename;
+        await addImageWatermarkToImage('./img/' + options.inputImage, './img/' + prepareOutputFilename(options.inputImage), './img/' + options.watermarkImage);
+    }
 };
 
-addTextWatermarkToImage('./test.jpg', './test-with-watermark.jpg', 'Hello world');
+startApp();
